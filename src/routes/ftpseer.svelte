41a182180ed576731d3e12b32b98ad02fc4bf40d
m3yevn <mmm.myintmyat@gmail.com>
2020-02-02 18:35:38 +0800

    :rocket: Attempt manual heroku

--- src/routes/ftpseer.svelte
+++ src/routes/ftpseer.svelte
@@ -42,7 +42,7 @@
         error.path = !thisPath;
       } else {
         fetch(
-          `http://${$page.query.host}:${$page.query.port}/ftpseer/directory?${
+          `https://${$page.query.host}:${$page.query.port}/ftpseer/directory?${
             username ? `username=${username}` : ""
           }&${password ? `password=${password}` : ""}&${
             port ? `port=${port}` : ""
